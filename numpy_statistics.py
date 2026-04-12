43dc029b6cf8286de63a78e706f9f13b95ad780d
Kunalthakur468 <103130740+Kunalthakur468@users.noreply.github.com>
2026-04-12 13:41:52 +0530

    Create numpy_statistics.py

---
+++ numpy_statistics.py
@@ -0,0 +1,16 @@
+import numpy as np
+arr = np.array([1,2,3,4,5,6,6,7])
+
+print(sum(arr)/len(arr))
+
+mean = np.mean(arr)
+print("mean is :",mean)
+
+std_dev = np.std(arr)
+print("standard devation is :",std_dev)
+
+median = np.median(arr)
+print("median is :",median)
+
+variance = np.var(arr)
+print("Variance is:",variance)
